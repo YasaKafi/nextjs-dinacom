6ba83a1bf8eb3e99aceb4e4d322b07fcf15448a8
Fadzaa <fadza20@gmail.com>
2023-12-30 23:21:32 +0700

    feat: [FA] add profile-training detail tab bar

--- src/app/(pages)/(feature-pages)/profile/page.jsx
+++ src/app/(pages)/(feature-pages)/profile/page.jsx
@@ -1,9 +1,12 @@
 import ProfileSection from "@/app/(pages)/(feature-pages)/profile/components/profile-section";
+import ProfileTrainingDetail
+    from "@/app/(pages)/(feature-pages)/profile/components/profile-training-detail/profile-training-detail";
 
 function ProfilePage() {
     return (
         <>
             <ProfileSection/>
+            <ProfileTrainingDetail/>
         </>
     );
 }
