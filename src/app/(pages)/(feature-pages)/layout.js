6df3055d505ff3f41902fcc2705e97afa97c5852
Fadzaa <fadza20@gmail.com>
2024-01-02 14:41:07 +0700

    feat: [FA] add header job page and fix navbar static

--- src/app/(pages)/(feature-pages)/layout.js
+++ src/app/(pages)/(feature-pages)/layout.js
@@ -2,6 +2,7 @@ import { Montserrat } from 'next/font/google'
 import '../../globals.css'
 import Footer from "@/app/components/footer";
 import Navbar from "@/app/components/navbar";
+import NavbarStatic from "@/app/components/navbar-static";
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <body className={montserrat.className}>
         <Navbar/>
+        <NavbarStatic/>
         {children}
         <Footer/>
         </body>
