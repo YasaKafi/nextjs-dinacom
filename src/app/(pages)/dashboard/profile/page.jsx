6b1077d79ef9bbc75c098a85f8d10ba9e2f17169
Fadzaa <fadza20@gmail.com>
2024-01-03 13:38:29 +0700

    fix minor

--- src/app/(pages)/dashboard/profile/page.jsx
+++ src/app/(pages)/dashboard/profile/page.jsx
@@ -1,5 +1,7 @@
 import ProfileHeader from "@/app/(pages)/dashboard/profile/components/profile-header";
 import ProfileAbout from "@/app/(pages)/dashboard/profile/components/profile-about";
+import ProfileTrainingDetail
+    from "@/app/(pages)/(feature-pages)/profile/components/profile-training-detail/profile-training-detail";
 
 function Page() {
     return (
@@ -7,6 +9,7 @@ function Page() {
             <ProfileHeader/>
             <div className="flex w-full h-1/2 mt-10">
                 <ProfileAbout/>
+                <ProfileTrainingDetail/>
             </div>
         </div>
     );
