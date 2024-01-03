45321fc1a08f0861ac19002d882ac3299041a5e6
Fadzaa <fadza20@gmail.com>
2024-01-03 22:35:38 +0700

    feat: [FA] add search bar

--- src/app/(pages)/(feature-pages)/courses/page.jsx
+++ src/app/(pages)/(feature-pages)/courses/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import CourseHeader from "@/app/(pages)/(feature-pages)/courses/components/header";
+import SearchBarCourses from "@/app/(pages)/(feature-pages)/courses/components/search-bar";
 
 function Page() {
     return (
         <div className="h-screen w-screen">
             <CourseHeader/>
+            <div className="w-full h-full px-[80px] mt-20">
+                <SearchBarCourses/>
+            </div>
         </div>
     );
 }
