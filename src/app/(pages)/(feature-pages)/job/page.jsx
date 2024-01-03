092f34db9996d5594429afd49644eec4e22fb5a6
Fadzaa <fadza20@gmail.com>
2024-01-03 21:46:55 +0700

    feat: [FA] add search bar component

--- src/app/(pages)/(feature-pages)/job/page.jsx
+++ src/app/(pages)/(feature-pages)/job/page.jsx
@@ -1,9 +1,13 @@
 import Header from "@/app/(pages)/(feature-pages)/job/components/header";
+import SearchBar from "@/app/(pages)/(feature-pages)/job/components/search-bar";
 
 function JobPage() {
     return (
         <div className="h-screen w-screen">
             <Header/>
+            <div className="w-full h-full  my-[75px] px-[60px]">
+                <SearchBar/>
+            </div>
         </div>
     );
 }
