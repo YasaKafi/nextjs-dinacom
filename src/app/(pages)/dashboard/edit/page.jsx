2d4a1c2f80dac77deb7fdb809eb2c3ba49af14ed
Yaskrazz <macbook@Guramee.local>
2024-01-04 01:47:10 +0700

    feat : [YK] added profile edit in edit profile

--- src/app/(pages)/dashboard/edit/page.jsx
+++ src/app/(pages)/dashboard/edit/page.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import Image from "next/image";
-import { editProfileUser } from "@/app/lib/utils/images";
-import { FiEdit } from "react-icons/fi";
 import FormEdit from "./components/form-edit";
-import Form from "../../(initial-pages)/components/form";
 import RadioGender from "./components/radio-gender";
+import ProfileEdit from "./components/profile-edit";
 
 
 export default function EditProfilePage() {
@@ -19,16 +16,7 @@ export default function EditProfilePage() {
   return (
     <main className="w-full h-full ">
       <div className="w-full h-full relative flex flex-col lg:flex-row lg:justify-between pt-12 px-14">
-        <div className="w-52 h-52 relative rounded-full bg-orange-300">
-          <Image
-            src={editProfileUser}
-            alt="profile user"
-            className="object-cover w-full h-full"
-          />
-          <div className="absolute bottom-0 flex items-center justify-center right-0 w-16 h-16 rounded-full bg-blue-500">
-            <FiEdit size={25} color="white" />
-          </div>
-        </div>
+        <ProfileEdit/>
 
         <div className="w-3/4 h-full flex flex-col ">
           {labels.map((label, index) => (
