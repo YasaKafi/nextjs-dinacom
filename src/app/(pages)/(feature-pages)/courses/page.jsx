debd1f9f546b4011e1f34d82e60a9aa37d0d6809
Fadzaa <fadza20@gmail.com>
2024-01-03 22:30:25 +0700

    feat: [FA] add course header component

---
+++ src/app/(pages)/(feature-pages)/courses/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CourseHeader from "@/app/(pages)/(feature-pages)/courses/components/header";
+
+function Page() {
+    return (
+        <div className="h-screen w-screen">
+            <CourseHeader/>
+        </div>
+    );
+}
+
+export default Page;
