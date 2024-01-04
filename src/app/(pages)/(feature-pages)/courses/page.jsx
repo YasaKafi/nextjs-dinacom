7a0c11b6831d6292372435c61ea6ed89a34251f0
Fadzaa <fadza20@gmail.com>
2024-01-04 08:15:30 +0700

    feat: [FA] add course fetch

--- src/app/(pages)/(feature-pages)/courses/page.jsx
+++ src/app/(pages)/(feature-pages)/courses/page.jsx
@@ -3,8 +3,9 @@ import CourseHeader from "@/app/(pages)/(feature-pages)/courses/components/heade
 import SearchBarCourses from "@/app/(pages)/(feature-pages)/courses/components/search-bar";
 import Image from "next/image";
 import {courseHeaderBanner} from "@/app/lib/utils/images";
+import course from "@/app/lib/services/course";
 
-function Page() {
+async function Page() {
     const data = [
         {
             id:1,
@@ -55,6 +56,10 @@ function Page() {
         }
     ]
 
+    const fetchCourse = await course()
+
+    console.log(fetchCourse)
+
     return (
         <div className="w-screen bg-gray-100 ">
             <CourseHeader/>
@@ -62,18 +67,16 @@ function Page() {
                 <SearchBarCourses/>
                 <div className="h-full grid grid-cols-4 gap-5 mt-10">
 
-                    {data.map((item,index)=> (
+                    {fetchCourse.map((item,index)=> (
                         <div className="rounded-xl h-[400px] bg-white overflow-hidden mb-5">
 
-                            <Image className="w-full h-[130px]" src={courseHeaderBanner} alt="Course Header Banner"
+                            <Image className="w-full h-[130px] object-cover" src={item.image_course} alt="Course Header Banner"
                                    width={300} height={300}/>
 
                             <div className="flex flex-col h-[270px] justify-between gap-12 px-5 py-4">
 
-                                <h1 className="text-primary font-bold text-lg">Product Management Basic</h1>
-                                <p className="text-textPrimary font-light text-xs">Product Management Masterclass, you
-                                    will learn with Sarah Johnson - Head of Product Customer Platform Gojek
-                                    Indonesia.</p>
+                                <h1 className="text-primary font-bold text-lg">{item.title}</h1>
+                                <p className="text-textPrimary font-light text-xs">{item.description}</p>
 
                                 <button className="bg-primary w-2/3 py-3 text-white rounded-xl">
                                     Daftar Sekarang
