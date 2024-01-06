3477cd0f65766f8e8ab260ea984bcf7c42841a71
Yaskrazz <macbook@Guramee.local>
2024-01-06 23:12:05 +0700

    feat : [YK] add hero component in enroll courses page

---
+++ src/app/(pages)/(feature-pages)/courses/courses-enroll/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import HeroComponent from "./components/hero-component";
+
+export default function EnrollCourses() {
+  return (
+    <main className="w-full">
+      <HeroComponent />
+    </main>
+  );
+}
--- src/app/(pages)/(feature-pages)/layout.js
+++ src/app/(pages)/(feature-pages)/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <NavbarStatic/>
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
