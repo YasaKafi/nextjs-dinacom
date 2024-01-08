237550560c29086a7f995e57e89eba92197de788
Fadzaa <fadza20@gmail.com>
2024-01-08 20:11:11 +0700

    feat: [FA] add header internship enroll

---
+++ src/app/(pages)/(feature-pages)/(feat)/internship/internship-enroll/page.jsx
@@ -0,0 +1,13 @@
+import HeaderInternshipEnroll
+    from "@/app/(pages)/(feature-pages)/(feat)/internship/internship-enroll/components/header";
+
+function Page() {
+    return (
+        <div className="w-screen h-screen">
+            <HeaderInternshipEnroll/>
+
+        </div>
+    );
+}
+
+export default Page;
