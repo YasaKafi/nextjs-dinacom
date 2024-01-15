931efc667f6c162e9001654e9991396dfdb17ebe
Fadzaa <fadza20@gmail.com>
2024-01-15 17:28:09 +0700

    feat: [FA] add link and id segmen detail job

--- src/app/(pages)/(feature-pages)/(feat)/job/[id]/page.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/job/[id]/page.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import DetailHero from "./detail-component/detail-hero";
 import DetailDescription from "./detail-component/detail-description";
+import jobDetail from "@/app/lib/services/endpoint/api/job-detail";
 
 export default async function DetailPage({params}) {
-    
+    const id = params.id;
+    const fetchJob = await jobDetail(id)
+
     return(
         <>
+            Tes{id}
             <DetailHero />
             <DetailDescription/>
         </>
