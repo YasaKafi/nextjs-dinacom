24b6870928767d02d643988971197c212ee95133
Fadzaa <fadza20@gmail.com>
2024-01-17 20:53:44 +0700

    fix: [FA] fix detail job fetch

--- src/app/(pages)/(feature-pages)/(feat)/job/[id]/detail-component/detail-hero.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/job/[id]/detail-component/detail-hero.jsx
@@ -3,14 +3,10 @@ import Image from "next/image";
 import { bgHeroDetail } from "@/app/lib/utils/svg";
 import jobDetail from "@/app/lib/services/endpoint/api/job-detail";
 
-
-async function getDataJob() {
-  const jobData = await jobDetail(id);
-  return jobData.data;
-}
-
-export default async function DetailHero() {
-  const dataJob = await getDataJob();
+export default async function DetailHero({id}) {
+  const jobData = await jobDetail(id)
+  console.log("JOB DATA TESTING")
+    console.log(jobData)
   return (
     <div className=" w-full  flex items-center justify-start ">
       <Image
@@ -21,14 +17,14 @@ export default async function DetailHero() {
         className="object-cover w-full h-[300px]"
       ></Image>
       <div className="absolute lg:top-32 left-[35%] lg:left-[30%] top-32 text-white lg:text-5xl text-2xl  md:text-3xl font-semibold">
-      {dataJob.jobdesk}
+      {jobData.jobdesk}
       </div>
       <Image width={100}
-        height={100} src={dataJob.company_image} alt="logoCompany" className="absolute lg:top-28 left-[10%] top-32 w-1/5 lg:w-[15%] 2xl:w-[10%] rounded-xl">
+        height={100} src={jobData.company_image} alt="logoCompany" className="absolute lg:top-28 left-[10%] top-32 w-1/5 lg:w-[15%] 2xl:w-[10%] rounded-xl">
         
       </Image>
       <div className="absolute lg:top-48 left-[35%] lg:left-[30%] top-44 text-white lg:text-2xl text-lg sm:text-xl font-normal">
-        {dataJob.company_name}
+        {jobData.company_name}
       </div>
     </div>
   );
--- src/app/lib/services/endpoint/api/job-detail.js
+++ src/app/lib/services/endpoint/api/job-detail.js
@@ -3,7 +3,7 @@ import instanceAxios from "@/app/lib/services/instance/instance";
 export default async function jobDetail(id) {
     try {
       const response = await instanceAxios.get(`/job/detail/${id}`); 
-      return response.data; 
+      return response.data.data;
     } catch (error) {
       console.error(error);
       throw error;
