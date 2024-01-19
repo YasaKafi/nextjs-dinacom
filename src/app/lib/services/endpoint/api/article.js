8dde0d42b230b0603c43af05506b6971877ab41f
Fadzaa <fadza20@gmail.com>
2024-01-20 00:39:27 +0700

    feat: [FA] add filtering chip article

--- src/app/lib/services/endpoint/api/article.js
+++ src/app/lib/services/endpoint/api/article.js
@@ -1,10 +1,13 @@
 import instanceAxios from "@/app/lib/services/instance/instance";
 
-export default async function article(query) {
+export default async function article(query, category) {
     try {
         const res = await instanceAxios.get(
             `/search/article`,
-            {params: {search: query}
+            {params: {
+                search: query,
+                category: category
+            }
             }
         )
         return res.data.data
