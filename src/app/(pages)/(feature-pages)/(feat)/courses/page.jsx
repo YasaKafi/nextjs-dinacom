70f2b96bf6c1e4a5c6e649865a61119429597f98
Nareswara05 <naresalhaq9@gmail.com>
2024-01-20 21:44:13 +0700

    feat [NR] fix card course layout

--- src/app/(pages)/(feature-pages)/(feat)/courses/page.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/courses/page.jsx
@@ -6,6 +6,7 @@ import course from "@/app/lib/services/endpoint/api/course";
 import CourseHeader from "@/app/(pages)/(feature-pages)/(feat)/courses/components/header";
 import SearchBarCourses from "@/app/(pages)/(feature-pages)/(feat)/courses/components/search-bar";
 import Link from "next/link";
+import { iconPremium, internshipBanner, premiumIcons } from "@/app/lib/utils/svg";
 
 async function Page({ searchParams }) {
   const search =
@@ -26,16 +27,20 @@ async function Page({ searchParams }) {
               key={index}
               className="rounded-xl relative  bg-white overflow-hidden mb-5"
             >
-              <div className={`absolute top-6 left-4 py-2 px-4 ${
-                item.difficulty === "begginer"
-                  ? "bg-green-600"
-                  : item.difficulty === "intermediate"
-                  ? "bg-blue-600"
-                  : item.difficulty === "expert"
-                  ? "bg-red-600"
-                  : "" 
-              } rounded-lg z-10`}>
-                <h3 className="font-medium text-sm text-white">{item.difficulty}</h3>
+              <div
+                className={`absolute top-6 left-4 py-2 px-4 ${
+                  item.difficulty === "begginer"
+                    ? "bg-green-600"
+                    : item.difficulty === "intermediate"
+                    ? "bg-blue-600"
+                    : item.difficulty === "expert"
+                    ? "bg-red-600"
+                    : ""
+                } rounded-lg z-10`}
+              >
+                <h3 className="font-medium text-sm text-white">
+                  {item.difficulty}
+                </h3>
               </div>
 
               <Image
@@ -46,18 +51,83 @@ async function Page({ searchParams }) {
                 height={300}
               />
 
-              <div className="flex flex-col  justify-between gap-12 px-5 py-4">
-                <h2 className="w-[40%] flex items-center justify-center py-2 rounded-xl border border-primary text-primary"> {item.category} </h2>
+              <div className="flex flex-col  justify-between gap-8 px-5 py-4 w-full">
+                <div>
+                  <div className="flex flex-col gap-2">
+                    <h4 className=" px-3 py-2 rounded-xl text-sm border border-primary text-primary w-fit">
+                      {" "}
+                      {item.category}{" "}
+                    </h4>
 
-                
-                <h1 className="text-primary font-bold text-lg">{item.title}</h1>
-                <p className="text-textPrimary font-light text-xs">
-                  {item.description}
-                </p>
+                    <h1 className="text-primary font-bold text-2xl truncate hover:text-clip">
+                      {item.title}
+                    </h1>
+                  </div>
+                  <p className="text-textPrimary font-light h-[50px] text-xs  hover:text-clip truncate">
+                    {item.description}
+                  </p>
+                </div>
 
                 <Link href={`/courses/detail/${item.id}`}>
                   <button className="bg-primary w-2/3 py-3 text-white rounded-xl">
                     Daftar Sekarang
+                  </button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="h-full grid grid-cols-4 gap-5 mt-10">
+          {fetchCourse.map((item, index) => (
+            <div
+              key={index}
+              className="rounded-xl relative  bg-white overflow-hidden mb-5"
+            >
+              <div
+                className={`absolute top-6 left-4 py-2 px-4 ${
+                  item.difficulty === "begginer"
+                    ? "bg-green-600"
+                    : item.difficulty === "intermediate"
+                    ? "bg-blue-600"
+                    : item.difficulty === "expert"
+                    ? "bg-red-600"
+                    : ""
+                } rounded-lg z-10`}
+              >
+                <h3 className="font-medium text-sm text-white">
+                  {item.difficulty}
+                </h3>
+              </div>
+
+              <Image
+                className="w-full h-[130px] object-cover"
+                src={item.image_course}
+                alt="Course Header Banner"
+                width={300}
+                height={300}
+              />
+
+              <div className="flex flex-col  justify-between gap-8 px-5 py-4 w-full">
+                <div>
+                  <div className="flex flex-col gap-2">
+                    <h4 className=" px-3 py-2 rounded-xl text-sm border border-premium text-premium w-fit">
+                      {" "}
+                      {item.category}{" "}
+                    </h4>
+
+                    <h1 className="text-premium font-bold text-2xl truncate hover:text-clip">
+                      {item.title}
+                    </h1>
+                  </div>
+                  <p className="text-textPrimary font-light h-[50px] text-xs  hover:text-clip truncate">
+                    {item.description}
+                  </p>
+                </div>
+
+                <Link href={`/courses/detail/${item.id}`}>
+                  <button className="bg-gradient-to-r from-premium via-yellow-200 to-yellow-300 w-2/3 py-3 text-white rounded-xl flex items-center font-semibold justify-center gap-2">
+                    Premium
+                  <Image src={premiumIcons}/>
                   </button>
                 </Link>
               </div>
